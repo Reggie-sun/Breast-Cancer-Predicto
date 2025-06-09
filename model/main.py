cfa25197d02b8e0374227a04c7af008a0752d835
Reggie-sun <sunruijie42@gmail.com>
2025-06-09 22:30:07 +0800

    Update main.py

--- model/main.py
+++ model/main.py
@@ -32,7 +32,7 @@ def create_model(data):
 
 def get_clean_data():
     data = pd.read_csv(
-       "data\data.csv")
+       "data/data.csv")
     data = data.drop(columns=['Unnamed: 32', 'id'])
     data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
 
